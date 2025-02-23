c428086cd242e38b67d2487d9c97d3ac46196089
Antonio <antoniokerrrocks@gmail.com>
2025-02-23 14:28:36 -0500

    Finished Raid class: add new constructor for police deployment and update arrest logic

--- uwiJava/javaProjectOne/src/Raid.java
+++ uwiJava/javaProjectOne/src/Raid.java
@@ -4,21 +4,44 @@ public class Raid extends Operation {
         Services service = Services.getService();
         return service.policeAvailable(numCriminalsAdjusted);
     }
-    private int numOfArrests; // Raid #1
+    
+    private int numOfArrests; // Part 1(1) record # of arrests
 
     public Raid(Community community) {
         super(community);
+        ArrayList<Criminal> criminals = community.getCriminals();  
+        // Part 1(2) SET # OF ARRESTS TO MATCH # OF CRIMINALS 
+        this.numOfArrests = criminals.size();
 
+        // Part 1(5)
+        for (Criminal criminal : criminals) {
+            criminal.arrest(); 
+        }
     }
+
+    // Part 1(3) New Constructor
+    public Raid(Community community, int multiplier) {
+        super(community);
+        Services service = Services.getService();
+        ArrayList<Criminal> criminals = community.getCriminals();
+
+        // DEPLOY REQUIRED POLICE
+        int requiredPolice = community.countCriminals() * multiplier;
+        service.deployPolice(requiredPolice);
+
+        // SET # OF ARRESTS TO MATCH # OF CRIMINALS 
+        this.numOfArrests = criminals.size();
+
+        // ARREST ALL CRIMINALS 
+        for (Criminal criminal : criminals) {
+            criminal.arrest();
+        }
+    }
+
+
 
     public int countArrests() {
-       return numOfArrests; // Raid #2
-
-    }
-
-    public int matchArrests(Community community, int multiplier) {
-        this.numOfArrests = cm.getCriminals() * multiplier;
-        return this.numOfArrests;
+       return numOfArrests; // Part 1(4)
     }
 
     public String toString() {

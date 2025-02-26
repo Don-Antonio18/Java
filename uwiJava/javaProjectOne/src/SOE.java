889712688ef3d3f09c384529112cda3d980ce475
Antonio <antoniokerrrocks@gmail.com>
2025-02-25 23:14:09 -0500

    Refactor SOE class constructor and methods for clarity and functionality

--- uwiJava/javaProjectOne/src/SOE.java
+++ uwiJava/javaProjectOne/src/SOE.java
@@ -3,35 +3,43 @@ import java.util.ArrayList;
 public class SOE extends ZOSO {
     private int rehabNum; // PART 4(1)
 
-    /*
-     * rehabilitated number is the ( numCriminals * rehab rate ) / 100
-     */
+         // PART 4(3)
+    public SOE(Community community, int multiplier, int rehabRate) {
+            super(community, multiplier);
+            this.rehabNum = (rehabRate * community.countCriminals()) / 100;
 
-    // PART 4(2)
-    public static boolean canDeploy(int deploymentSize) {
+            // get deployment service for social worker workers
+            Services service = Services.getService();
+            service.deploySocial(multiplier * community.countCriminals());
+
+            // get rehabilitations
+            this.rehabNum = (int) (community.countCriminals() * rehabRate) / 100;
+
+            for (int i = 0; i < rehabNum && i < community.countCriminals(); i++) {
+                // get criminal 
+                Criminal criminal = community.getCriminals().get(i);
+                // create new resident with criminal's name
+                new Resident(criminal.getName(), community);
+                // rehabilitate criminal
+                criminal.rehabilitate();
+
+    
+
+            }
+    }
+    
+        
+    
+
+    // PART 4(2) - renamed to avoid shadowing
+    public static boolean canDeploSOE(int deploymentSize) {
         Services service = Services.getService();
         return ( service.soldiersAvailable(deploymentSize) &&
                     service.socialAvailable(deploymentSize) &&
                         service.policeAvailable(2) 
-        );
+                );
     }
-
-    // PART 4(3)
-
-    public SOE(Community community, int multipler, int rehabRate) {
-        super(community, multipler);
-
-        // get list of criminals in community
-        ArrayList<Criminal> criminals = community.getCriminals(); 
-        this.rehabNum = (int) (criminals.size() * rehabRate) / 100;
-
-
-         for (int i = 0; i < rehabRate; i++) {
-             Criminal criminal = criminals.get(i);
-             Resident resident = new Resident(criminal.getName(), community);
-                criminal.rehabilitate();
-         }
-    }
+     
 
     // PART 4(4)
     public int countRehabs(){
